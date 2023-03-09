28c1baa2c7e596bec307c20fcb1d680c94b0528a
leduygit <leduyanh04@gmail.com>
2023-03-09 14:59:21 +0700

    adding fonts

--- main.cpp
+++ main.cpp
@@ -3,8 +3,29 @@
 #include <SFML/Network.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
+#include "Components/Button.hpp"
 using namespace std;
 
 int main(){
-    
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "Visualize algo");
+    sf::Font consolas;
+    consolas.loadFromFile("Font/Consolas.ttf");
+
+    Button testButton("Testing", {100, 60}, 10, sf::Color::White, sf::Color::Green);
+    testButton.setPosition({80, 80});
+    testButton.setFont(consolas);
+
+    while (window.isOpen()){
+        sf::Event ev;
+        while (window.pollEvent(ev))
+        {
+            if (ev.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        //Button testButton;
+        testButton.drawButton(window);
+        window.display();
+    }
 }
