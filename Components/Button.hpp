56d6f229bb650fdc2d17e4c1e90dd08c98c70fe8
leduygit <leduyanh04@gmail.com>
2023-03-09 12:56:35 +0700

    adding public functions for button

--- Components/Button.hpp
+++ Components/Button.hpp
@@ -30,6 +30,25 @@ public:
 
     void setPosition(sf::Vector2f pos){
         button.setPosition(pos);
+
+        float xPos = (pos.x + button.getGlobalBounds().width / 2) - (text.getGlobalBounds().width / 2);
+        float yPos = (pos.y + button.getGlobalBounds().height / 2) - (text.getGlobalBounds().height / 2);
+        text.setPosition({xPos, yPos});
+    }
+
+    void drawButton(sf::RenderWindow &window){
+        window.draw(button);
+        window.draw(text);
+    }
+
+    bool isMouseOnButton(sf::RenderWindow &window){
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+        sf::Vector2f buttonPos = button.getPosition();
+
+        sf::Vector2f buttonSize = button.getSize();
+        return (mousePos.x >= buttonPos.x && mousePos.x <= buttonPos.x + buttonSize.x
+             && mousePos.y >= buttonPos.y && mousePos.y <= buttonPos.y + buttonSize.y);
     }
 private:
     sf::RectangleShape button;
