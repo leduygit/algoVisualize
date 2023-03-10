237d601d02e6a4cd3c895a75194092b0de61bf6e
leduygit <leduyanh04@gmail.com>
2023-03-10 12:16:22 +0700

    updating main and button

--- main.cpp
+++ main.cpp
@@ -11,21 +11,37 @@ int main(){
     sf::Font consolas;
     consolas.loadFromFile("Font/Consolas.ttf");
 
-    Button testButton("Testing", {100, 60}, 10, sf::Color::White, sf::Color::Green);
-    testButton.setPosition({80, 80});
+    Button testButton("Testing", {150, 60}, 20, sf::Color::White, sf::Color::Blue);
+    testButton.setPosition({750, 450});
     testButton.setFont(consolas);
+
+    Button button2("Duy Anh", {300, 150}, 30, sf::Color::White, sf::Color::Blue);
+    button2.setPosition({100, 100});
+    button2.setFont(consolas);
 
     while (window.isOpen()){
         sf::Event ev;
         while (window.pollEvent(ev))
         {
-            if (ev.type == sf::Event::Closed)
+            switch (ev.type)
+            {
+            case (sf::Event::Closed):
                 window.close();
+                break;
+            case (sf::Event::MouseMoved):
+                if (testButton.isMouseOnButton(window))
+                    testButton.setBackColor(sf::Color::Green);
+                else
+                    testButton.setBackColor(sf::Color::White);
+            default:
+                break;
+            }
         }
 
         window.clear();
         //Button testButton;
         testButton.drawButton(window);
+        button2.drawButton(window);
         window.display();
     }
 }
