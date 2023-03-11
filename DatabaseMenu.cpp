c6c42ebeaa001bc7d5585e96aba1dc2853a368a0
leduygit <leduyanh04@gmail.com>
2023-03-11 11:36:36 +0700

    update database menu

---
+++ DatabaseMenu.cpp
@@ -0,0 +1,62 @@
+#include "DatabaseMenu.hpp"
+
+void DatabaseMenu::setStaticButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    staticArrayButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setDynamicButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    dynamicArrayButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setSinglyButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    singlyLinkedListButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setDoublyButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    doublyLinkedListButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setCircularButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    circularLinkedListButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setStackButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    stackButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setQueueButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    queueButton = Button(str, textColor, font, buttonSize, backgroundColor);
+}
+
+void DatabaseMenu::setupDatabaseMenu(sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor)
+{
+    staticArrayButton = Button("Static Array", textColor, font, buttonSize, backgroundColor);
+    dynamicArrayButton = Button("Dynamic Array", textColor, font, buttonSize, backgroundColor);
+    singlyLinkedListButton = Button("Singly Linked List", textColor, font, buttonSize, backgroundColor);
+    doublyLinkedListButton = Button("Doubly Linked List", textColor, font, buttonSize, backgroundColor);
+    circularLinkedListButton = Button("Circular Linked List", textColor, font, buttonSize, backgroundColor);
+    stackButton = Button("Stack", textColor, font, buttonSize, backgroundColor);
+    queueButton = Button("Queue", textColor, font, buttonSize, backgroundColor);
+    backButton = Button("Return", textColor, font, buttonSize, backgroundColor);
+}
+
+
+
+void DatabaseMenu::drawDatabaseMenu(sf::RenderWindow &window)
+{
+    staticArrayButton.drawButton(window);
+    dynamicArrayButton.drawButton(window);
+    singlyLinkedListButton.drawButton(window);
+    doublyLinkedListButton.drawButton(window);
+    circularLinkedListButton.drawButton(window);
+    stackButton.drawButton(window);
+    queueButton.drawButton(window);
+    backButton.drawButton(window);
+}
--- DatabaseMenu.hpp
+++ DatabaseMenu.hpp
@@ -3,13 +3,25 @@
 #include "Button.hpp"
 class DatabaseMenu{
 public:
+    DatabaseMenu() {};
+    void setStaticButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setDynamicButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setSinglyButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setDoublyButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setCircularButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setStackButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void setQueueButton(std::string str, sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void DatabaseMenu::setupDatabaseMenu(sf::Color textColor, sf::Font &font, sf::Vector2f buttonSize, sf::Color backgroundColor);
+    void drawDatabaseMenu(sf::RenderWindow &window);
+    void setPosition(sf::Vector2f pos);
 private:
-    Button staticArrayButton = Button();
-    Button dynamicArrayButton = Button();
-    Button stackButton = Button();
-    Button queueButton = Button();
-    Button singlyLinkedListButton = Button();
-    Button doublyLinkedListButton = Button();
-    Button circularLinkedListButton = Button();
+    Button staticArrayButton = Button(); // ID = 4
+    Button dynamicArrayButton = Button(); // ID = 5
+    Button singlyLinkedListButton = Button(); // ID = 6
+    Button doublyLinkedListButton = Button(); // ID = 7
+    Button circularLinkedListButton = Button(); // ID = 8
+    Button stackButton = Button(); // ID = 9
+    Button queueButton = Button(); // ID = 10
+    Button backButton = Button();
 };
 #endif
